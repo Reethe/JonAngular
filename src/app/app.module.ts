37bcce79826b29b9e4eb23fe33c98d261a0a557b
Reethe <volvagiasword@sbcglobal.net>
2019-12-02 09:40:02 -0500

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { BoxComponent } from './box/box.component';
 import { Gen1Component } from './gen1/gen1.component';
+import { PokedisplayComponent } from './pokedisplay/pokedisplay.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, BoxComponent, Gen1Component ],
+  declarations: [ AppComponent, HelloComponent, BoxComponent, Gen1Component, PokedisplayComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/pokedisplay/pokedisplay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokedisplay',
+  templateUrl: './pokedisplay.component.html',
+  styleUrls: ['./pokedisplay.component.css']
+})
+export class PokedisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
