742ab776a381ce1b9ce32f823bd9d5dcda9fefd2
Reethe <volvagiasword@sbcglobal.net>
2019-12-02 09:32:59 -0500

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { BoxComponent } from './box/box.component';
+import { Gen1Component } from './gen1/gen1.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, BoxComponent ],
+  declarations: [ AppComponent, HelloComponent, BoxComponent, Gen1Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/gen1/gen1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gen1',
+  templateUrl: './gen1.component.html',
+  styleUrls: ['./gen1.component.css']
+})
+export class Gen1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
